fbfec517ad859a94162ad2eb1ccc67ef75d13ae1
Lucasmercado101 <lucasmercado101@gmail.com>
2021-11-24 17:45:58 -0300

    added submit button w/styles and added Required True to inputs

--- src/Pages/Register.elm
+++ src/Pages/Register.elm
@@ -1,8 +1,8 @@
 module Pages.Register exposing (Model, Msg, init, update, view)
 
 import Browser
-import Html exposing (div, form, input, label, text)
-import Html.Attributes exposing (class, for, id, placeholder, type_, value)
+import Html exposing (button, div, form, input, label, text)
+import Html.Attributes exposing (class, classList, disabled, for, id, placeholder, required, type_, value)
 import Html.Events exposing (onInput)
 
 
@@ -36,12 +36,17 @@ view : Model -> Browser.Document Msg
 view { password, username } =
     { title = "Register"
     , body =
+        let
+            registerDisabled =
+                password == "" || username == ""
+        in
         [ form [ class "flex flex-col container mx-auto px-4 gap-y-4" ]
             [ div []
                 [ label [ class "block text-gray-700 text-sm font-bold mb-2", for "username" ]
                     [ text "Username" ]
                 , input
-                    [ value username
+                    [ required True
+                    , value username
                     , onInput ChangeUsername
                     , class "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                     , id "username"
@@ -54,7 +59,8 @@ view { password, username } =
                 [ label [ class "block text-gray-700 text-sm font-bold mb-2", for "password" ]
                     [ text "Password" ]
                 , input
-                    [ value password
+                    [ required True
+                    , value password
                     , onInput ChangePassword
                     , class "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                     , id "password"
@@ -63,6 +69,16 @@ view { password, username } =
                     ]
                     []
                 ]
+            , button
+                [ type_ "submit"
+                , class "font-bold py-2 px-4 rounded"
+                , classList
+                    [ ( "bg-blue-700 hover:bg-blue-700 text-white", not registerDisabled )
+                    , ( "bg-gray-300 hover:bg-gray-400 text-gray-800", registerDisabled )
+                    ]
+                , disabled registerDisabled
+                ]
+                [ text "Register" ]
             ]
         ]
     }
