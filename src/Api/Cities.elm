a3fdc70f945d96743401ebef83c974def589a1f8
Lucasmercado101 <lucasmercado101@gmail.com>
2021-11-25 18:45:42 -0300

    created postNewCity fn

--- src/Api/Cities.elm
+++ src/Api/Cities.elm
@@ -1,24 +1,9 @@
-module Api.Cities exposing (City, getCities)
+module Api.Cities exposing (City, getCities, postNewCity)
 
 import Api.Common exposing (baseUrl)
-import Http
-import Json.Decode exposing (Decoder, field, int, list, map3, string)
-
-
-getCities : (Result Http.Error (List City) -> msg) -> Cmd msg
-getCities msg =
-    Http.get
-        { url = baseUrl ++ "/cities"
-        , expect = Http.expectJson msg (list cityDecoder)
-        }
-
-
-cityDecoder : Decoder City
-cityDecoder =
-    map3 City
-        (field "id" int)
-        (field "name" string)
-        (field "country" string)
+import Http exposing (jsonBody, post)
+import Json.Decode as JD exposing (Decoder)
+import Json.Encode as JE
 
 
 type alias City =
@@ -26,3 +11,37 @@ type alias City =
     , name : String
     , country : String
     }
+
+
+getCities : (Result Http.Error (List City) -> msg) -> Cmd msg
+getCities msg =
+    Http.get
+        { url = baseUrl ++ "/cities"
+        , expect = Http.expectJson msg (JD.list cityDecoder)
+        }
+
+
+postNewCity : (Result Http.Error City -> msg) -> City -> Cmd msg
+postNewCity msg newCityData =
+    post
+        { url = baseUrl ++ "/cities"
+        , body = jsonBody <| cityEncoder newCityData
+        , expect = Http.expectJson msg cityDecoder
+        }
+
+
+cityDecoder : Decoder City
+cityDecoder =
+    JD.map3 City
+        (JD.field "id" JD.int)
+        (JD.field "name" JD.string)
+        (JD.field "country" JD.string)
+
+
+cityEncoder : City -> JE.Value
+cityEncoder { country, id, name } =
+    JE.object
+        [ ( "id", JE.int id )
+        , ( "name", JE.string name )
+        , ( "country", JE.string country )
+        ]
