a89b2880843253a71b7ce7ae94ac3156a3319789
Lucasmercado101 <lucasmercado101@gmail.com>
2021-11-26 14:57:06 -0300

    added boilerplate to add styles and state to the form

--- src/Pages/Cities.elm
+++ src/Pages/Cities.elm
@@ -2,9 +2,9 @@ module Pages.Cities exposing (Model, Msg, init, subscriptions, update, view)
 
 import Api.Cities exposing (City)
 import Browser
-import Html exposing (Html, a, button, div, h1, h3, img, li, p, span, text, ul)
-import Html.Attributes exposing (class, href, src, type_)
-import Html.Events exposing (onClick)
+import Html exposing (Html, a, button, div, form, h1, h2, h3, img, input, label, li, p, span, text, ul)
+import Html.Attributes exposing (class, classList, disabled, for, href, id, placeholder, required, src, type_, value)
+import Html.Events exposing (onClick, onInput)
 import Http
 import Platform.Cmd exposing (Cmd)
 import Session
@@ -13,6 +13,11 @@ import Session
 type alias Model =
     { citiesData : CitiesData
     , userData : Maybe Session.UserData
+
+    -- new city data
+    , isNewCityModalOpen : Bool
+    , newCityName : String
+    , newCityCountry : String
     }
 
 
@@ -26,6 +31,10 @@ type Msg
     = GotCities (Result Http.Error (List City))
     | RetryFetchCities
     | GotUserData (Maybe Session.UserData)
+      -- Modal
+    | ToggleModal
+    | ChangeCityName String
+    | ChangeCountryName String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -45,11 +54,24 @@ update msg model =
         GotUserData userData ->
             ( { model | userData = userData }, Cmd.none )
 
+        -- Modal
+        ToggleModal ->
+            ( { model | isNewCityModalOpen = not model.isNewCityModalOpen }, Cmd.none )
+
+        ChangeCityName newCityName ->
+            ( { model | newCityName = newCityName }, Cmd.none )
+
+        ChangeCountryName newCityCountry ->
+            ( { model | newCityCountry = newCityCountry }, Cmd.none )
+
 
 init : ( Model, Cmd Msg )
 init =
     ( { citiesData = Loading
       , userData = Nothing
+      , isNewCityModalOpen = False
+      , newCityName = ""
+      , newCityCountry = ""
       }
     , Cmd.batch
         [ Api.Cities.getCities GotCities
@@ -59,22 +81,28 @@ init =
 
 
 subscriptions : Model -> Sub Msg
-subscriptions model =
+subscriptions _ =
     Sub.map GotUserData Session.localStorageUserSub
 
 
 view : Model -> Browser.Document Msg
-view model =
+view ({ citiesData, isNewCityModalOpen } as model) =
     { title = "Cities"
     , body =
-        [ -- modal
-          -- ,
-          h1 [ class "text-center font-semibold text-3xl my-4" ] [ text "Cities" ]
+        [ if isNewCityModalOpen then
+            modal model
 
-        -- , button
-        --     [ class "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" ]
-        --     [ text "Retry" ]
-        , case model.citiesData of
+          else
+            text ""
+        , h1 [ class "text-center font-semibold text-3xl my-4" ] [ text "Cities" ]
+        , div [ class "px-2 mb-2" ]
+            [ button
+                [ onClick ToggleModal
+                , class "mx-auto w-full md:w-64 block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                ]
+                [ text "Add City" ]
+            ]
+        , case citiesData of
             Loading ->
                 div [ class "text-xl text-center" ] [ text "Loading..." ]
 
@@ -115,32 +143,67 @@ city cityData =
         ]
 
 
-modal : Html msg
-modal =
+modal : Model -> Html Msg
+modal { newCityName, newCityCountry } =
+    let
+        registerDisabled =
+            newCityName == "" || newCityCountry == ""
+    in
     div [ class "fixed z-50 inset-0 overflow-y-auto" ]
         [ div [ class "flex items-center justify-center min-h-screen px-4 text-center sm:block sm:p-0" ]
-            [ div [ class "fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" ]
+            [ div [ class "fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity", onClick ToggleModal ]
                 []
             , span [ class "hidden sm:inline-block sm:align-middle sm:h-screen" ]
                 [ text "\u{200B}" ]
-            , div [ class "inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" ]
-                [ div [ class "bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4" ]
-                    [ div [ class "sm:flex sm:items-start" ]
-                        [ div [ class "mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left" ]
-                            [ h3 [ class "text-lg leading-6 font-medium text-gray-900" ]
-                                [ text "Deactivate account" ]
-                            , div [ class "mt-2" ]
-                                [ p [ class "text-sm text-gray-500" ]
-                                    [ text "Are you sure you want to deactivate your account? All of your data will be permanently removed. This action cannot be undone.              " ]
-                                ]
-                            ]
-                        ]
+            , div [ class "py-6 inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" ]
+                [ h2 [ class "text-2xl text-center mb-4" ] [ text "New City" ]
+                , form
+                    [ -- onSubmit SubmitForm
+                      -- ,
+                      class
+                        "flex flex-col container mx-auto px-4 gap-y-4"
                     ]
-                , div [ class "bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse" ]
-                    [ button [ class "w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm", type_ "button" ]
-                        [ text "Deactivate        " ]
-                    , button [ class "mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm", type_ "button" ]
-                        [ text "Cancel        " ]
+                    [ div []
+                        [ label [ class "block text-gray-700 text-sm font-bold mb-2", for "city-name" ]
+                            [ text "Name" ]
+                        , input
+                            [ required True
+                            , value newCityName
+                            , onInput ChangeCityName
+                            , class "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            , placeholder "name"
+                            , id "city-name"
+                            , type_ "text"
+                            ]
+                            []
+                        ]
+                    , div []
+                        [ label [ class "block text-gray-700 text-sm font-bold mb-2", for "country" ]
+                            [ text "Country" ]
+                        , input
+                            [ required True
+                            , value newCityCountry
+                            , onInput ChangeCountryName
+                            , class "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            , id "country"
+                            , placeholder "country"
+                            , type_ "text"
+                            ]
+                            []
+                        ]
+                    , div [ class "flex ml-auto gap-x-4" ]
+                        [ button [ class "px-4 py-2", onClick ToggleModal ] [ text "Cancel" ]
+                        , button
+                            [ type_ "submit"
+                            , class "font-bold py-2 px-4 rounded"
+                            , classList
+                                [ ( "bg-blue-700 hover:bg-blue-700 text-white", not registerDisabled )
+                                , ( "bg-gray-300 hover:bg-gray-400 text-gray-800", registerDisabled )
+                                ]
+                            , disabled registerDisabled
+                            ]
+                            [ text "Create" ]
+                        ]
                     ]
                 ]
             ]
