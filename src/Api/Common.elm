e37a9e01a58adc6f1079e2f39ea5be5cd9f22c33
Lucasmercado101 <lucasmercado101@gmail.com>
2021-11-26 17:01:40 -0300

    abstracted riskyRequest to postWIthCredentials

--- src/Api/Common.elm
+++ src/Api/Common.elm
@@ -1,6 +1,40 @@
-module Api.Common exposing (baseUrl)
+module Api.Common exposing (baseUrl, postWithCredentials)
+
+import Http exposing (Body, Expect, Header, riskyRequest)
+
+
+requestBody :
+    Body
+    -> Expect msg
+    ->
+        { method : String
+        , headers : List Header
+        , url : String
+        , body : Body
+        , expect : Expect msg
+        , timeout : Maybe Float
+        , tracker : Maybe String
+        }
+requestBody body expect =
+    { method = "POST"
+    , url = baseUrl ++ "/cities"
+    , body = body
+    , expect = expect
+    , timeout = Nothing
+    , tracker = Nothing
+    , headers = []
+    }
+
+
+
+-- PUBLIC
 
 
 baseUrl : String
 baseUrl =
     "http://localhost:8001"
+
+
+postWithCredentials : Body -> Expect msg -> Cmd msg
+postWithCredentials body expect =
+    riskyRequest (requestBody body expect)
