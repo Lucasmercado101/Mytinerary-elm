2467d6797133b23ed96bf18379c4c5e136954c05
Lucasmercado101 <lucasmercado101@gmail.com>
2021-11-29 13:01:13 -0300

    WIP: verticalDots button shows if the itinerary is yours

--- src/Pages/City.elm
+++ src/Pages/City.elm
@@ -281,7 +281,7 @@ view ({ cityData, isCreatingNewItinerary } as model) =
 
                           else
                             ul [ class "container mx-auto px-4 pb-4 flex flex-col md:flex-row md:flex-wrap items-stretch" ]
-                                (List.map (\l -> li [ class "md:w-1/2 xl:w-1/3 w-full p-2" ] [ itinerary l ]) itineraries)
+                                (List.map (\l -> li [ class "md:w-1/2 xl:w-1/3 w-full p-2" ] [ itinerary l model ]) itineraries)
                         ]
                     ]
 
@@ -308,20 +308,34 @@ view ({ cityData, isCreatingNewItinerary } as model) =
     }
 
 
-itinerary : Itinerary -> Html msg
-itinerary data =
+itinerary : Itinerary -> Model -> Html msg
+itinerary data model =
     div [ class "mt-3 flex flex-col rounded shadow-sm p-3 bg-white md:h-full md:justify-between" ]
         [ div [ class "flex flex-row mb-2" ]
-            [ if data.creator.profilePic == Nothing then
+            ([ if data.creator.profilePic == Nothing then
                 div [ class "pointer-events-none w-12 h-12 bg-red-500 text-white capitalize rounded-full flex" ]
                     [ div [ class "flex w-12 h-12" ] [ p [ class "m-auto text-xl" ] [ text (String.left 1 data.creator.username) ] ]
                     ]
 
-              else
+               else
                 img [ class "pointer-events-none w-12 h-12 rounded-full" ] []
-            , h3 [ class "ml-3 self-center text-lg truncate" ] [ text data.title ]
-            , button [ class "w-12 h-12 ml-auto" ] [ div [ class "flex w-12 h-12" ] [ verticalDotsSvg ] ]
-            ]
+             , h3 [ class "ml-3 self-center text-lg truncate" ] [ text data.title ]
+             ]
+                ++ (case model.userSession of
+                        Just userData ->
+                            if userData.id == data.creator.id then
+                                [ button [ class "w-12 h-12 ml-auto" ]
+                                    [ div [ class "flex w-12 h-12" ] [ verticalDotsSvg ]
+                                    ]
+                                ]
+
+                            else
+                                []
+
+                        Nothing ->
+                            []
+                   )
+            )
         , div [ class "flex flex-grow" ]
             [ div [ class "flex-grow" ]
                 [ p [ class "font-semibold" ] [ text "Activities:" ]
