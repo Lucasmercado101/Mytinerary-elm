a9432d3676dd7059a9eae58686b3cea82d8ff75c
Lucasmercado101 <lucasmercado101@gmail.com>
2021-12-01 17:10:42 -0300

    added patchBody and patchWithCredentials

--- src/Api/Common.elm
+++ src/Api/Common.elm
@@ -1,4 +1,4 @@
-module Api.Common exposing (deleteWithCredentials, endpoint, postWithCredentials)
+module Api.Common exposing (deleteWithCredentials, endpoint, patchWithCredentials, postWithCredentials)
 
 import Http exposing (Body, Expect, Header, emptyBody, riskyRequest)
 
@@ -50,6 +50,30 @@ deleteBody expect endpointStr =
     }
 
 
+patchBody :
+    Body
+    -> Expect msg
+    -> String
+    ->
+        { method : String
+        , headers : List Header
+        , url : String
+        , body : Body
+        , expect : Expect msg
+        , timeout : Maybe Float
+        , tracker : Maybe String
+        }
+patchBody body expect endpointStr =
+    { method = "PATCH"
+    , url = endpointStr
+    , body = body
+    , expect = expect
+    , timeout = Nothing
+    , tracker = Nothing
+    , headers = []
+    }
+
+
 
 -- PUBLIC
 
@@ -67,6 +91,11 @@ postWithCredentials endpointStr body expect =
 deleteWithCredentials : String -> Expect msg -> Cmd msg
 deleteWithCredentials endpointStr expect =
     riskyRequest (deleteBody expect endpointStr)
+
+
+patchWithCredentials : String -> Body -> Expect msg -> Cmd msg
+patchWithCredentials endpointStr body expect =
+    riskyRequest (patchBody body expect endpointStr)
 
 
 endpoint : List String -> String
