ac8fc932a70a86af866d506cdcc23e9f14f258c4
Lucasmercado101 <lucasmercado101@gmail.com>
2021-12-03 19:23:53 -0300

    added styles

--- src/Pages/Register.elm
+++ src/Pages/Register.elm
@@ -3,11 +3,12 @@ module Pages.Register exposing (Model, Msg, init, update, view)
 import Api.Auth exposing (User)
 import Browser
 import Browser.Navigation as Nav
-import Html exposing (Html, button, div, form, input, label, p, text)
-import Html.Attributes exposing (class, classList, disabled, for, id, placeholder, required, type_, value)
+import Html exposing (Html, a, button, div, form, img, input, label, p, text)
+import Html.Attributes exposing (class, classList, disabled, for, href, id, placeholder, required, src, type_, value)
 import Html.Events exposing (onInput, onSubmit)
 import Http exposing (Error(..))
 import SvgIcons exposing (errorSvg)
+import TailwindHelpers as TW exposing (..)
 
 
 type alias Model =
@@ -42,6 +43,10 @@ init key =
     )
 
 
+
+-- https://source.unsplash.com/featured/vacation
+
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
@@ -63,10 +68,6 @@ update msg model =
                     ( { model | registeringState = Error err }, Cmd.none )
 
 
-
--- TODO  https://tailwindui.com/components/application-ui/forms/sign-in-forms#component-fef1d7e9849359af76d4d6ead796938b
-
-
 view : Model -> Browser.Document Msg
 view { password, username, registeringState } =
     { title = "Register"
@@ -75,67 +76,131 @@ view { password, username, registeringState } =
             registerDisabled =
                 password == "" || username == ""
         in
-        [ form
-            [ onSubmit SubmitForm
-            , class
-                "flex flex-col container mx-auto px-4 gap-y-4 pt-5"
-            ]
-            [ div []
-                [ label [ class "block text-gray-700 text-sm font-bold mb-2", for "username" ]
-                    [ text "Username" ]
-                , input
-                    [ required True
-                    , value username
-                    , onInput ChangeUsername
-                    , class "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    , id "username"
-                    , placeholder "Username"
-                    , type_ "text"
+        [ div [ class "flex", TW.apply [ h_full ] ]
+            [ div [ TW.apply [ w_full ], class "flex-1" ]
+                [ div
+                    [ TW.apply [ w_full, h_full, relative ]
+                    , class "sm:block hidden"
                     ]
-                    []
-                ]
-            , div []
-                [ label [ class "block text-gray-700 text-sm font-bold mb-2", for "password" ]
-                    [ text "Password" ]
-                , input
-                    [ required True
-                    , value password
-                    , onInput ChangePassword
-                    , class "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    , id "password"
-                    , placeholder "Password"
-                    , type_ "text"
+                    [ img
+                        [ src "/assets/registerImage.jpg"
+                        , class "object-cover object-left h-full sm:block hidden"
+                        , TW.apply [ w_full, h_full ]
+                        ]
+                        []
+                    , div
+                        [ TW.apply
+                            [ absolute
+                            , bottom_0
+                            , left_0
+                            , bg_black
+                            , w_full
+                            , bg_opacity_75
+                            ]
+                        ]
+                        [ div [ TW.apply [ p_2, text_white ] ]
+                            [ text "Photo by "
+                            , a [ TW.apply [ text_purple_400 ], href "https://unsplash.com/@seb_crdt?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" ]
+                                [ text "sebastien cordat" ]
+                            , text " on "
+                            , a [ TW.apply [ text_purple_400 ], href "https://unsplash.com/s/photos/trip-city?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" ]
+                                [ text "Unsplash" ]
+                            ]
+                        ]
                     ]
-                    []
                 ]
-            , button
-                [ type_ "submit"
-                , class "font-bold py-2 px-4 rounded"
-                , classList
-                    [ ( "bg-blue-700 hover:bg-blue-700 text-white", not registerDisabled )
-                    , ( "bg-gray-300 hover:bg-gray-400 text-gray-800", registerDisabled )
+            , div
+                [ TW.apply
+                    [ w_full
+                    , h_full
+                    , sm [ max_w_xl, px_16 ]
+                    , md [ max_w_2xl ]
+                    , lg [ max_w_3xl ]
                     ]
-                , disabled registerDisabled
+                , class "flex"
                 ]
-                [ text "Register" ]
-            , case registeringState of
-                Idle ->
-                    empty
+                [ div
+                    [ class "flex flex-col"
+                    , TW.apply
+                        [ w_full
+                        , m_auto
+                        , gap_y_4
+                        , p_6
+                        ]
+                    ]
+                    [ p
+                        [ TW.apply
+                            [ md [ text_4xl ]
+                            , text_3xl
+                            , font_semibold
+                            ]
+                        ]
+                        [ text "Register a new account" ]
+                    , form
+                        [ onSubmit SubmitForm
+                        , class
+                            "flex flex-col shadow-sm"
+                        , TW.apply [ gap_y_4 ]
+                        ]
+                        [ div []
+                            [ label [ class "block text-gray-700 text-sm font-bold mb-2", for "username" ]
+                                [ text "Username" ]
+                            , input
+                                [ required True
+                                , value username
+                                , onInput ChangeUsername
+                                , class "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                , id "username"
+                                , placeholder "Username"
+                                , type_ "text"
+                                ]
+                                []
+                            ]
+                        , div []
+                            [ label [ class "block text-gray-700 text-sm font-bold mb-2", for "password" ]
+                                [ text "Password" ]
+                            , input
+                                [ required True
+                                , value password
+                                , onInput ChangePassword
+                                , class "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                , id "password"
+                                , placeholder "Password"
+                                , type_ "password"
+                                ]
+                                []
+                            ]
+                        , button
+                            [ type_ "submit"
+                            , class "font-bold py-2 px-4 rounded"
+                            , classList
+                                [ ( "bg-blue-700 hover:bg-blue-700 text-white", not registerDisabled )
+                                , ( "bg-gray-300 hover:bg-gray-400 text-gray-800", registerDisabled )
+                                ]
+                            , disabled registerDisabled
+                            ]
+                            [ text "Register" ]
+                        , case registeringState of
+                            Idle ->
+                                empty
 
-                Registering ->
-                    empty
+                            Registering ->
+                                empty
 
-                Error err ->
-                    case err of
-                        BadStatus int ->
-                            if int == 409 then
-                                error [ text ("Username \"" ++ username ++ "\" is already taken.") ]
+                            Error err ->
+                                case err of
+                                    BadStatus int ->
+                                        if int == 409 then
+                                            error [ text ("Username \"" ++ username ++ "\" is already taken.") ]
 
-                            else
-                                error [ text ("Bad status code: " ++ String.fromInt int) ]
+                                        else
+                                            error [ text ("Bad status code: " ++ String.fromInt int) ]
 
-                        _ ->
-                            error [ text "Unknown error" ]
+                                    _ ->
+                                        error [ text "Unknown error" ]
+                        ]
+                    ]
+                ]
             ]
         ]
     }
