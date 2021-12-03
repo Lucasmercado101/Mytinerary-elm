9c12a9cf716547843b0a4addd57e76b45b4cb1c9
Lucasmercado101 <lucasmercado101@gmail.com>
2021-12-03 19:35:34 -0300

    added marMarkerOutlineSvg

--- src/SvgIcons.elm
+++ src/SvgIcons.elm
@@ -135,3 +135,29 @@ errorSvg =
             ]
             []
         ]
+
+
+mapMarkerOutlineSvg : List (Svg.Attribute msg) -> Html msg
+mapMarkerOutlineSvg attrs =
+    Svg.svg
+        ([ Svg.Attributes.fill "none"
+         , Svg.Attributes.viewBox "0 0 24 24"
+         , Svg.Attributes.stroke "currentColor"
+         ]
+            ++ attrs
+        )
+        [ Svg.path
+            [ Svg.Attributes.strokeLinecap "round"
+            , Svg.Attributes.strokeLinejoin "round"
+            , Svg.Attributes.strokeWidth "2"
+            , Svg.Attributes.d "M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+            ]
+            []
+        , Svg.path
+            [ Svg.Attributes.strokeLinecap "round"
+            , Svg.Attributes.strokeLinejoin "round"
+            , Svg.Attributes.strokeWidth "2"
+            , Svg.Attributes.d "M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+            ]
+            []
+        ]
